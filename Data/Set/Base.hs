42ab5405e4c988eba25f627d0df133b27e4ce42b
Peter Selinger <selinger@mathstat.dal.ca>
2014-07-04 10:47:35 -0300

    Fixed syntax of fixity declarations.

--- Data/Set/Base.hs
+++ Data/Set/Base.hs
@@ -362,7 +362,7 @@ member = go
 {-# INLINE member #-}
 #endif
 
-infix 4 member
+infix 4 `member`
 
 -- | /O(log n)/. Is the element not in the set?
 notMember :: Ord a => a -> Set a -> Bool
@@ -373,7 +373,7 @@ notMember a t = not $ member a t
 {-# INLINE notMember #-}
 #endif
 
-infix 4 notMember
+infix 4 `notMember`
 
 -- | /O(log n)/. Find largest element smaller than the given one.
 --
@@ -626,7 +626,7 @@ union t1 t2 = hedgeUnion NothingS NothingS t1 t2
 {-# INLINABLE union #-}
 #endif
 
-infixl 5 union
+infixl 5 `union`
 
 hedgeUnion :: Ord a => MaybeS a -> MaybeS a -> Set a -> Set a -> Set a
 hedgeUnion _   _   t1  Tip = t1
@@ -686,7 +686,7 @@ intersection t1 t2 = hedgeInt NothingS NothingS t1 t2
 {-# INLINABLE intersection #-}
 #endif
 
-infixl 5 intersection
+infixl 5 `intersection`
 
 hedgeInt :: Ord a => MaybeS a -> MaybeS a -> Set a -> Set a -> Set a
 hedgeInt _ _ _   Tip = Tip
